0aae02ce9b88e136db7a4db42c145e18f6f695bd
donguri-py <turntabroz@gmail.com>
2023-07-02 23:00:07 +0900

    isalphaメソッド

---
+++ 0702_11.py
@@ -0,0 +1,15 @@
+x = "Python"
+print(x.isalpha())
+# 出力内容 True
+
+y = "ハローワールド"
+print(y.isalpha())
+# 出力内容 True
+
+z = "毎日学習"
+print(z.isalpha())
+# 出力内容 True
+
+aaa = "健康が1番"
+print(aaa.isalpha())
+# 出力内容 False
