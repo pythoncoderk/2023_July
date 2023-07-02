c112cd43933db766fd52df3c774f24afbdb08ec5
donguri-py <turntabroz@gmail.com>
2023-07-02 22:51:46 +0900

    isalnumメソッド

---
+++ 0702_10.py
@@ -0,0 +1,19 @@
+x = "Python"
+print(x.isalnum())
+# 出力結果 True
+
+y = "ABC123"
+print(y.isalnum())
+# 出力結果 True
+
+z = "毎日勉強"
+print(z.isalnum())
+# 出力結果 True
+
+aaa = "Hello World!"
+print(aaa.isalnum())
+# 出力結果 False
+
+bbb = ""
+print(bbb.isalnum())
+# 出力結果 False
