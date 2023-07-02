eac26fdce1c1c49c43b9f941a4f138c87376b735
donguri-py <turntabroz@gmail.com>
2023-07-02 21:13:31 +0900

    型ヒント

--- 0702_8.py
+++ 0702_8.py
@@ -3,6 +3,5 @@ n = input()
 for i in range(len(n)):
     if i % 3 == 0 and i != 0:
         print(",", end="")
-
     print(n[i], end="")
-print()
+# print()
---
+++ 0702_9.py
@@ -0,0 +1,6 @@
+# 引数 name に int を指定
+# 戻り値 に int を指定
+def output(name: int) -> int:
+    return "Hello, " + name
+
+print(output("TARO"))
