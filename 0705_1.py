6d2ebeffb5814c7053e1a6371ad393b757f040b8
donguri-py <turntabroz@gmail.com>
2023-07-05 07:27:11 +0900

    ininstance関数

---
+++ 0705_1.py
@@ -0,0 +1,11 @@
+print(isinstance(100, int))
+# 出力結果 True
+
+print(isinstance(100, str))
+# 出力結果 False
+
+print(isinstance("ABCDE", str))
+# 出力結果 True
+
+print(isinstance("ABCDE", float))
+# 出力結果 False
