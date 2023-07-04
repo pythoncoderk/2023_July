f6723f468fa43a3d840d53e1b0299cb0ca36a35e
donguri-py <turntabroz@gmail.com>
2023-07-05 06:57:31 +0900

    ジェネレータ

--- 0704_4.py
+++ 0704_4.py
@@ -1,4 +1,10 @@
-import re
+def multiplier(values):
+    ret = []
+    for i in values:
+        ret.append(2 ** i)
+    return ret
 
-m = re.match("a.c", "abc")
-print(m.group())
+values = [0, 1, 2, 3, 4, 5]
+ret = multiplier(values)
+print(type(ret))
+print(ret)
