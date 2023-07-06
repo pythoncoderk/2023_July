6be4619c4a783d21b12231f421fd1d3ac579dd7c
donguri-py <turntabroz@gmail.com>
2023-07-06 21:26:06 +0900

    zip関数

---
+++ 0706_4.py
@@ -0,0 +1,4 @@
+def func(a, b, c):
+    print(a, b, c)
+
+func(a=1, b=2, c=4)
---
+++ 0706_7.py
@@ -0,0 +1,5 @@
+names = ["太郎", "花子", "学"]
+ages = [15, 41, 66]
+zipped = zip(names, ages)
+print(zipped)
+print(list(zipped))
