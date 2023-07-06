0f6520c939e51cfa4ab2aef120326da26a5dadd4
donguri-py <turntabroz@gmail.com>
2023-07-06 23:58:02 +0900

    リスト内包表記

---
+++ 0706_8.py
@@ -0,0 +1,2 @@
+even = [i if i % 2 == 0 else None for i in range(10)]
+print(even)
---
+++ 0706_9.py
@@ -0,0 +1,9 @@
+l1 = []
+for i in range(10):
+    l1.append(i)
+
+print(l1)
+
+l2 = [i for i in range(10)]
+
+print(l2)
