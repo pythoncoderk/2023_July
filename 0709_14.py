2b3b180c2a5db051394320df6097a5ca1ecdfef1
donguri-py <turntabroz@gmail.com>
2023-07-09 19:45:42 +0900

    zip()関数

---
+++ 0709_14.py
@@ -0,0 +1,6 @@
+it1 = (1, 2, 3)
+it2 = ["abc", "ABC", "123"]
+it3 = "あいう"
+
+for v in zip(it1, it2, it3):
+    print(v)
---
+++ 0709_15.py
@@ -0,0 +1,5 @@
+it1 = (1, 2, 3, 4, 5)
+it2 = ["abc", "ABC", "123"]
+for v in zip(it1, it2):
+    print(v)
+
