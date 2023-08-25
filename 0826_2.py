f3d51031172cc5651d174be90f110ec11bc7a3c9
donguri-py <turntabroz@gmail.com>
2023-08-26 08:09:19 +0900

    文字列

---
+++ 0826_2.py
@@ -0,0 +1,22 @@
+print("hello")
+print("hello")
+print('I don\'t know')
+print("say \"I don\'t know\"")
+
+print("hello.\nHow are you?")
+
+print(r"C:\name\name")
+
+print("#####################")
+print("""\
+line
+line
+line\
+""")
+print("#####################")
+
+print("Hi." * 3 + "Mike.")
+print("Py""thon")
+
+prefix = "Py"
+print(prefix + "thon")
