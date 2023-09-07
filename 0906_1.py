7fb6cc5c3254f2849c9546259834c0d49bc1ad19
donguri-py <turntabroz@gmail.com>
2023-09-08 00:01:27 +0900

    doctest

--- 0906_1.py
+++ 0906_1.py
@@ -1,2 +1,21 @@
-n = input()
-print(n.count("A"))
+class Cal(object):
+    def add_num_and_double(self, x, y):
+        """Add and double
+
+        >>> c = Cal()
+        >>> c.add_num_and_double(1, 1)
+        4
+
+        >>> c.add_num_and_double("1", "1")
+        4
+        :param x:
+        :param y:
+        :return:
+        """
+        result = x + y
+        result *= 2
+        return result
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
