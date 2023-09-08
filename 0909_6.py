a444012059516046fc269e49661ab5e282955336
donguri-py <turntabroz@gmail.com>
2023-09-09 08:12:08 +0900

    print4

---
+++ 0909_6.py
@@ -0,0 +1,14 @@
+print("a is {}".format("X"))
+print("a is {}".format("un"))
+print("a is {2} {1} {0}".format(1, 2, 3))
+print("my name is {0} {1}".format("ABE", "Hiroki"))
+print("My name is {name} {family}. Watashi ha {family} {name}".format(name="taro", family="yamada"))
+
+a = "a"
+print(f"a is {a}")
+
+x = 5
+y = 109
+z = 777
+
+print(f"{x} {y} {z}")
