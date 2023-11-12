8e55a97a7c6a6484aaf7dd469bcc8d14c43423ed
donguri-py <turntabroz@gmail.com>
2023-11-13 08:02:59 +0900

    文字列操作

---
+++ 1113_2.py
@@ -0,0 +1,22 @@
+s = "test"
+print(s)
+
+print("say \"I don't know\"")
+print("hello,\nHow are you?")
+print(r"C:\name\name")
+
+print("##########################")
+print("""\
+line1
+line2
+line3\
+""")
+print("##########################")
+
+print("Hi." * 3 + "Mike")
+print("Py""thon")
+
+s = ("aaaaaaaaaaaaaaaaaaaaaaaaaaa"
+     "tttttttttttttttttttttttttttt"
+     "hhhhhhhhhhhhhhhhhhhhhhhhhh")
+print(s)
