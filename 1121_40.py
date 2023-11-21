cc3af516c7068a4923e77e21d70d8e113a36123b
donguri-py <turntabroz@gmail.com>
2023-11-22 05:36:24 +0900

    標準入力

---
+++ 1121_40.py
@@ -0,0 +1,5 @@
+n = int(input())
+if n < 1000:
+    print(0)
+else:
+    print(n//10)
---
+++ 1121_44.py
@@ -0,0 +1,7 @@
+s = input()
+n = input()
+m = int(input())
+if s[m-1] == n:
+    print("Yes")
+else:
+    print("No")
