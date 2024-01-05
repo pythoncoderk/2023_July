e3c838832417d6f20de2abbddcb8296e17f20770
donguri-py <turntabroz@gmail.com>
2024-01-05 15:23:33 +0900

    標準入力

---
+++ 0105_15.py
@@ -0,0 +1,5 @@
+l = list(map(int, input().split()))
+if (l[1] / l[0]) * 100 >= 30.0:
+    print("Yes")
+else:
+    print("No")
---
+++ 0105_16.py
@@ -0,0 +1,6 @@
+a, b = map(str, input().split())
+a = int(a)
+if a >= 30 or b == "rainy":
+    print("Yes")
+else:
+    print("No")
