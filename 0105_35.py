763249c8fe69f09aeafb3d617fb0880ef6cb041f
donguri-py <turntabroz@gmail.com>
2024-01-06 07:40:36 +0900

    標準入力

---
+++ 0105_35.py
@@ -0,0 +1,22 @@
+# round関数を使った出力
+
+print(round(0.1))
+# 出力結果 0
+
+print(round(0.2))
+# 出力結果 0
+
+print(round(0.3))
+# 出力結果 0
+
+print(round(0.4))
+# 出力結果 0
+
+print(round(0.5))
+# 出力結果 0
+
+print(round(0.6))
+# 出力結果 1
+
+print(round(0.9))
+# 出力結果 1
