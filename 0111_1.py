a742279fc36c9f373eacb1c1135d241e5ab9c478
donguri-py <turntabroz@gmail.com>
2024-01-11 07:23:30 +0900

    標準入力

---
+++ 0111_1.py
@@ -0,0 +1,12 @@
+s = input()
+
+if s == "rose":
+    print("I love you")
+elif s == "cosmos":
+    print("harmony")
+elif s == "tulip":
+    print("perfect lover")
+elif s == "hydrangea":
+    print("heartlessness")
+elif s == "jasmine":
+    print("amiability")
