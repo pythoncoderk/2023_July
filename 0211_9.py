712e82fa6427b5765e181404adfbe8bef0d524bb
donguri-py <turntabroz@gmail.com>
2024-02-12 07:51:50 +0900

    標準入力

---
+++ 0211_9.py
@@ -0,0 +1,51 @@
+import copy
+
+n, a, b = map(int, input().split())
+l = [list(input()) for i in range(a)]
+l2 = copy.deepcopy(l)
+# print(n, a, b)
+# print(l)
+
+while l2[0]:
+    if len(l2[1]) != 0:
+        if l2[0][0] == l2[1][0]:
+            del l2[1][0]
+        del l2[0][0]
+    else:
+        break
+# print(l2)
+
+x = len(l2[1])
+
+l3 = copy.deepcopy(l)
+while l3[0]:
+    if len(l3[2]) != 0:
+        if l3[0][0] == l3[2][0]:
+            del l3[2][0]
+        del l3[0][0]
+    else:
+        break
+# print(l3)
+
+y = len(l3[2])
+
+print(f"{x} {y}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 0212_1.py
@@ -0,0 +1,5 @@
+N, A, B = map(int, input().split())
+l_A = [list(input()) for i in range(A)]
+
+print(N, A, B)
+print(l_A)
