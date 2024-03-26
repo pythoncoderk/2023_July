33751fc5ecb6c0a7e5ecf2606936e2f8f58bde5f
donguri-py <turntabroz@gmail.com>
2024-03-27 06:51:44 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ 0327_57.py
@@ -0,0 +1,7 @@
+n = input()
+m = input()
+
+if len(n) == len(m):
+    print("Yes")
+else:
+    print("No")
---
+++ 0327_58.py
@@ -0,0 +1,6 @@
+n = int(input())
+
+if n >= 1000:
+    print(n - 100)
+else:
+    print(n)
