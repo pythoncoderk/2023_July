9650cc4aba4ddad35c90a40e9fa219a7eaedfa1d
donguri-py <turntabroz@gmail.com>
2024-04-06 05:11:38 +0900

    Merge remote-tracking branch 'origin/master'

--- 0406_2.py
+++ 0406_2.py
@@ -1,3 +1,19 @@
-x, y = map(int, input().split())
+x, y = map(str, input().split())
 
-print(x, y)
+# print(x, y)
+
+x_l = list(x)
+y_l = list(y)
+
+x_l2 = [int(x_l[i]) for i in range(len(x_l))]
+y_l2 = [int(y_l[i]) for i in range(len(y_l))]
+
+x_sum = str(sum(x_l2))[-1]
+y_sum = str(sum(y_l2))[-1]
+
+if x_sum == y_sum:
+    print("Draw")
+elif x_sum < y_sum:
+    print("Alice")
+else:
+    print("Bob")
