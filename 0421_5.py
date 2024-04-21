e0b798e3394b5b5d10e1e12c4c6eec7abae00bc3
donguri-py <turntabroz@gmail.com>
2024-04-21 17:24:50 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ 0421_5.py
@@ -0,0 +1,9 @@
+n, r = map(int, input().split())
+l = [list(map(int, input().split())) for i in range(n)]
+
+# print(n, r)
+# print(l)
+
+for i in range(n):
+    if min(l[i]) >= r*2:
+        print(i+1)
