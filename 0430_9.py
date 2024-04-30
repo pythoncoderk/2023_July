cfb019fc962fd107e4f16a32187f5f165cf0d17a
donguri-py <turntabroz@gmail.com>
2024-04-30 13:23:59 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ 0430_9.py
@@ -0,0 +1,24 @@
+n = int(input())
+l = [list(map(int, input().split())) for i in range(n)]
+
+# print(n)
+# print(l)
+
+l2 = [sum(l[i]) for i in range(n)]
+
+# print(l2)
+
+l3 = [l2.count(l2[i]) for i in range(n)]
+
+# print(l3)
+
+max_l = max(l3)
+
+l4 = [l2[i] for i in range(n) if l3[i] == max_l]
+
+# print(l4)
+
+l4 = set(l4)
+l4 = list(l4)
+l4.sort()
+print(l4[0])
