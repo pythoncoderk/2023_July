347d3dadcd59b8c0dbf735bdfa10029be6b1a7b4
donguri-py <turntabroz@gmail.com>
2024-05-11 04:30:14 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ 0510_7.py
@@ -0,0 +1,10 @@
+h, w = map(int, input().split())
+l = [list(map(int, input().split())) for i in range(h)]
+q = int(input())
+l2 = [list(map(int, input().split())) for i in range(q)]
+
+print(h, w)
+print(l)
+print(q)
+print(l2)
+
