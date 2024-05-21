44e4488e21e03b997af18193da5086582eb9f1d8
donguri-py <turntabroz@gmail.com>
2024-05-21 21:45:21 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ 0521_6.py
@@ -0,0 +1,10 @@
+n = int(input())
+l = [list(map(str, input().split())) for i in range(n)]
+
+for i in range(n):
+    print("User{")
+    print(f"nickname : {l[i][0]}")
+    print(f"old : {l[i][1]}")
+    print(f"birth : {l[i][2]}")
+    print(f"state : {l[i][3]}")
+    print("}")
