9ad9185bbb9ac3c79f0b1030d444cba32c37df25
donguri-py <turntabroz@gmail.com>
2024-07-01 06:33:25 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ 0630_3.py
@@ -0,0 +1,8 @@
+n = int(input())
+l = list(map(int, input().split()))
+l2 = [list(map(int, input().split())) for i in range(n - 1)]
+
+print(n)
+print(l)
+print(l2)
+
---
+++ 0630_4.py
@@ -0,0 +1,6 @@
+s = input()
+
+x = s.index("R")
+y = s.index("M")
+
+print("Yes" if x < y else "No")
