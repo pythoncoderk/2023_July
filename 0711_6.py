dbd94000a891aa7f1b3e349b76e6f9137a40cbd4
donguri-py <turntabroz@gmail.com>
2024-07-11 07:32:44 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ 0711_6.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+if 1800 <= n <= 1999:
+    print(1)
+elif 1600 <= n <= 1799:
+    print(2)
+elif 1400 <= n <= 1599:
+    print(3)
+elif 1200 <= n <= 1399:
+    print(4)
+elif 1000 <= n <= 1199:
+    print(5)
+elif 800 <= n <= 999:
+    print(6)
+elif 600 <= n <= 799:
+    print(7)
+else:
+    print(8)
