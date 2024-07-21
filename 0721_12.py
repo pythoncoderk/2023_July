20f1ccfe928e7a0dd4c12d4b89a76518a9fcdb62
donguri-py <turntabroz@gmail.com>
2024-07-22 06:27:53 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ 0721_12.py
@@ -0,0 +1,7 @@
+s = input()
+
+l = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+x = l.index(s)
+
+print(5 - x)
---
+++ 0721_9.py
@@ -0,0 +1,7 @@
+l, r = map(int, input().split())
+
+# print(l, r)
+
+s = "atcoder"
+
+print(s[l - 1:r])
