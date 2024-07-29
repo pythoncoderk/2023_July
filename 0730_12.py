76529ddd3116e732d3631078ccc2a7e854863c7c
donguri-py <turntabroz@gmail.com>
2024-07-30 07:50:49 +0900

    Merge remote-tracking branch 'origin/master'

--- 0730_12.py
+++ 0730_12.py
@@ -2,10 +2,11 @@ n, m = map(int, input().split())
 l = list(map(int, input().split()))
 l2 = list(map(int, input().split()))
 
+count = 0
 for i in range(m):
     for j in range(l2[i]):
-        x = l.pop(0)
         if j == l2[i] - 1:
-            print(x)
+            print(l[count])
         else:
-            print(x, end=" ")
+            print(l[count], end=" ")
+        count += 1
