22ca1fd4f230b4bfdbab1ee6b8f99c7eaa597629
donguri-py <turntabroz@gmail.com>
2024-08-23 06:46:45 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ 0822_9.py
@@ -0,0 +1,19 @@
+d, k = map(int, input().split())
+
+print((d - 3) * k + 500 if d > 3 else 500)
+
+
+# n = int(input())
+# m = int(input())
+# k = int(input())
+#
+# print(n + m + k)
+#
+#
+# n, m, k = map(int, input().split())
+#
+# print(n, m, k)
+# print(n + m + k)
+# print(n * m * k)
+#
+# print( if  else )
