e3801cb58856e6cdb64562d0a9a424ab53a713f5
pythoncoderk <turntabroz@gmail.com>
2024-09-16 14:10:27 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ 0916_7.py
@@ -0,0 +1,14 @@
+l = [2, 5, 1, 4, 6, 3]
+
+for i in range(1, len(l)):
+    tmp = l[i]
+    for j in range(i - 1, -1, -1):
+        if l[j] > tmp:
+            l[j + 1] = l[j]
+        else:
+            l[j + 1] = tmp
+            break
+    else:
+        l[0] = tmp
+
+print(l)
